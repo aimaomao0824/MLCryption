e2311123382b9a3ffbeb967a727625e04e57087c
mtapple <>
2018-01-03 17:54:58 +0800

    Base64加密类
    完成Base64加密类，还未测试

--- MLCryptionDemo/MLCryptionDemo/NSObject+MLCryptionDES.m
+++ MLCryptionDemo/MLCryptionDemo/NSObject+MLCryptionDES.m
@@ -7,30 +7,177 @@
 //
 
 #import "NSObject+MLCryptionDES.h"
+#import <CommonCrypto/CommonCryptor.h>
 
 @implementation NSObject (MLCryptionDES)
-
-+ (NSString *)ml_test_NSObject
-{
-    return @"ml_test_NSObject这是一个测试方法";
-}
-- (NSString *)ml_test_NSObject
-{
-    return @"ml_test_NSObject这是一个测试方法";
-}
 
 @end
 
 @implementation NSString (MLCryptionDES)
 
-+ (NSString *)ml_test
-{
-    return @"这是一个测试方法";
-}
-- (NSString *)ml_test
-{
-    return @"这是一个测试方法";
+#pragma mark - DES加密类方法
++ (NSString *)ml_encryptUseDes:(NSString *)plainText key:(NSString *)key iv:(const void *)iv
+{    NSString *ciphertext = nil;
+    NSData *textData = [plainText dataUsingEncoding:NSUTF8StringEncoding];
+    NSUInteger dataLength = [textData length];
+    unsigned char buffer[1024];
+    memset(buffer, 0, sizeof(char));
+    size_t numBytesEncrypted = 0;
+    CCCryptorStatus cryptStatus = CCCrypt(kCCEncrypt, kCCAlgorithmDES,kCCOptionPKCS7Padding,[key UTF8String], kCCKeySizeDES,iv,[textData bytes], dataLength,buffer, 1024,&numBytesEncrypted);
+    if (cryptStatus == kCCSuccess) {
+        NSData *data = [NSData dataWithBytes:buffer length:(NSUInteger)numBytesEncrypted];
+        ciphertext = [self encode:data];
+    }
+    return ciphertext;
 }
 
+#pragma mark - DES加密对象方法
+- (NSString *)ml_encryptUseDesKey:(NSString *)key iv:(const void *)iv
+{
+    return [NSString ml_encryptUseDes:self key:key iv:iv];
+}
+
+#pragma mark - DES解密类方法
++ (NSString *)ml_decryptUseDes:(NSString *)cipherText key:(NSString *)key iv:(const void *)iv;
+{
+    NSString *plaintext = nil;
+    NSData *cipherdata = [self decode:cipherText];
+    unsigned char buffer[1024];
+    memset(buffer, 0, sizeof(char));
+    size_t numBytesDecrypted = 0;
+    CCCryptorStatus cryptStatus = CCCrypt(kCCDecrypt, kCCAlgorithmDES,
+                                          kCCOptionPKCS7Padding,
+                                          [key UTF8String], kCCKeySizeDES,
+                                          iv,
+                                          [cipherdata bytes], [cipherdata length],
+                                          buffer, 1024,
+                                          &numBytesDecrypted);
+    if(cryptStatus == kCCSuccess) {
+        NSData *plaindata = [NSData dataWithBytes:buffer length:(NSUInteger)numBytesDecrypted];
+        plaintext = [[NSString alloc]initWithData:plaindata encoding:NSUTF8StringEncoding];
+    }
+    return plaintext;
+}
+#pragma mark - DES解密对象方法
+- (NSString *)ml_decryptUseDesKey:(NSString *)key iv:(const void *)iv
+{
+    return [NSString ml_decryptUseDes:self key:key iv:iv];
+}
+
+
+static const char encodingTable[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
++ (NSString *)encode:(NSData *)data
+{
+    if (data.length == 0)
+        return nil;
+    
+    char *characters = malloc(data.length * 3 / 2);
+    
+    if (characters == NULL)
+        return nil;
+    
+    int end = (int)(data.length - 3);
+    int index = 0;
+    int charCount = 0;
+    int n = 0;
+    
+    while (index <= end) {
+        int d = (((int)(((char *)[data bytes])[index]) & 0x0ff) << 16)
+        | (((int)(((char *)[data bytes])[index + 1]) & 0x0ff) << 8)
+        | ((int)(((char *)[data bytes])[index + 2]) & 0x0ff);
+        
+        characters[charCount++] = encodingTable[(d >> 18) & 63];
+        characters[charCount++] = encodingTable[(d >> 12) & 63];
+        characters[charCount++] = encodingTable[(d >> 6) & 63];
+        characters[charCount++] = encodingTable[d & 63];
+        
+        index += 3;
+        
+        if(n++ >= 14)
+        {
+            n = 0;
+            characters[charCount++] = ' ';
+        }
+    }
+    
+    if(index == data.length - 2)
+    {
+        int d = (((int)(((char *)[data bytes])[index]) & 0x0ff) << 16)
+        | (((int)(((char *)[data bytes])[index + 1]) & 255) << 8);
+        characters[charCount++] = encodingTable[(d >> 18) & 63];
+        characters[charCount++] = encodingTable[(d >> 12) & 63];
+        characters[charCount++] = encodingTable[(d >> 6) & 63];
+        characters[charCount++] = '=';
+    }
+    else if(index == data.length - 1)
+    {
+        int d = ((int)(((char *)[data bytes])[index]) & 0x0ff) << 16;
+        characters[charCount++] = encodingTable[(d >> 18) & 63];
+        characters[charCount++] = encodingTable[(d >> 12) & 63];
+        characters[charCount++] = '=';
+        characters[charCount++] = '=';
+    }
+    NSString * rtnStr = [[NSString alloc] initWithBytesNoCopy:characters length:charCount encoding:NSUTF8StringEncoding freeWhenDone:YES];
+    return rtnStr;
+    
+}
+
++ (NSData *)decode:(NSString *)data
+{
+    if(data == nil || data.length <= 0) {
+        return nil;
+    }
+    NSMutableData *rtnData = [[NSMutableData alloc]init];
+    int slen = (int)data.length;
+    int index = 0;
+    while (true) {
+        while (index < slen && [data characterAtIndex:index] <= ' ') {
+            index++;
+        }
+        if (index >= slen || index  + 3 >= slen) {
+            break;
+        }
+        
+        int byte = ([self char2Int:[data characterAtIndex:index]] << 18) + ([self char2Int:[data characterAtIndex:index + 1]] << 12) + ([self char2Int:[data characterAtIndex:index + 2]] << 6) + [self char2Int:[data characterAtIndex:index + 3]];
+        Byte temp1 = (byte >> 16) & 255;
+        [rtnData appendBytes:&temp1 length:1];
+        if([data characterAtIndex:index + 2] == '=') {
+            break;
+        }
+        Byte temp2 = (byte >> 8) & 255;
+        [rtnData appendBytes:&temp2 length:1];
+        if([data characterAtIndex:index + 3] == '=') {
+            break;
+        }
+        Byte temp3 = byte & 255;
+        [rtnData appendBytes:&temp3 length:1];
+        index += 4;
+        
+    }
+    return rtnData;
+}
+
++ (int)char2Int:(char)c
+{
+    if (c >= 'A' && c <= 'Z') {
+        return c - 65;
+    } else if (c >= 'a' && c <= 'z') {
+        return c - 97 + 26;
+    } else if (c >= '0' && c <= '9') {
+        return c - 48 + 26 + 26;
+    } else {
+        switch(c) {
+            case '+':
+                return 62;
+            case '/':
+                return 63;
+            case '=':
+                return 0;
+            default:
+                return -1;
+        }
+    }
+}
 
 @end
--- MLCryptionDemo/MLCryptionDemo/ViewController.m
+++ MLCryptionDemo/MLCryptionDemo/ViewController.m
@@ -18,8 +18,44 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-//    [NSString ml_test];
-    [NSString ml];
+    [self testBase64_String];
+    
+    
+}
+
+-(void)testBase64_String
+{
+//    NSString *str = @"MannersLee";
+    NSString *str = @"中华人民共和国";
+    NSData *data = [[NSString alloc] initWithString:str];
+    NSLog(@"%@",data);
+    //类方法
+//    NSString *crypStr = [NSString ml_encodeStringUseBase64:str];
+//    NSString *deCrypStr = [NSString ml_encodeStringUseBase64:crypStr];
+    //对象方法
+    NSString *crypStr = [str ml_encodeStringUseBase64];
+    NSString *deCrypStr = [crypStr ml_decodeStringUseBase64];
+    
+//    NSLog(@"Base64-->>原文：%@，加密后：%@,解密后：%@",str,crypStr,deCrypStr);
+    
+//    NSData *crypStrData = [data ml_encodeDataUseBase64];
+//    NSData *deCrypStrData = [crypStrData ml_decodeDataUseBase64];
+//    NSLog(@"Base64-->>原文：%@，加密后：%@,解密后：%@",data,crypStrData,deCrypStrData);
+    
+}
+
+-(void)testDES
+{
+//    NSString *str = @"MannersLee";
+    NSString *str = @"中华人民共和国";
+    NSString *key = @"123456789012345678901234";
+    const Byte iv[] = {1,2,3,4,5,6,7,8};
+    //    NSString *crypStr = [NSString ml_encryptUseDes:str key:key iv: iv];
+    NSString *crypStr = [str ml_encryptUseDesKey:key iv:iv];
+    NSString *deCrypStr = [crypStr ml_decryptUseDesKey:key iv:iv];
+    //    NSString *deCrypStr = [NSString ml_decryptUseDes:crypStr key:key iv:iv];
+    NSLog(@"DES-->>原文：%@，key：%@，iv：%s，加密后：%@,解密后：%@",str,key,iv,crypStr,deCrypStr);
+    
 }
 
 - (void)didReceiveMemoryWarning {
