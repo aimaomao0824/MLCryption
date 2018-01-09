091c1ef87c2e44ee83c6e8b6195858cecadc9f6e
MannersLee <Manners@MannersLeeMacBookPro.local>
2018-01-09 23:26:54 +0800

    加密框架全部完成，等待测试

--- MLCryptionDemo/MLCryptionDemo/MLCryption/MLCryptionCommon.h
+++ MLCryptionDemo/MLCryptionDemo/MLCryption/MLCryptionCommon.h
@@ -13,26 +13,40 @@
 @interface MLCryptionCommon : NSObject
 
 
-/** MLMode */
+/** CCMode加密的模型如，CBC，ECB等 */
+@property (nonatomic, assign) CCMode mode;
+
+/** CCAlgorithm采用什么加密，如AES,DES等 */
 @property (nonatomic, assign) CCAlgorithm alg;
 
-/** MLMode */
-@property (nonatomic, assign) uint32_t MLMode;
+/** CCPadding填充方式，如ccPKCS7Padding，等 */
+@property (nonatomic, assign) CCPadding padding;
 
-/** MLPadding */
-@property (nonatomic, assign) CCPadding MLPadding;
-
-/** key */
-@property (nonatomic, copy) NSString *key;
-
-/** 密钥的长度位数MLKeySize */
-@property (nonatomic, assign) int keySize;
-
-/** iv */
+/** iv加密的向量 */
 @property (nonatomic, assign) const void *iv;
 
-/** 加密向量的长度ivSize */
+/** 加密向量的长度ivSize，如kMLBlockSizeAES128，kMLBlockSizeDES 等，多参数方法不用*/
 @property (nonatomic, assign) int ivSize;
+
+/** key 密钥*/
+@property (nonatomic, copy) NSString *key;
+
+/** 密钥的长度位数MLKeySize，如kMLKeySizeAES128，kMLKeySizeDES等 */
+@property (nonatomic, assign) size_t keySize;
+
+/** tweak 密钥*/
+@property (nonatomic, copy) NSString *tweak;
+
+/** 密钥的长度位数MLKeySize，如kMLKeySizeAES128，kMLKeySizeDES等 */
+@property (nonatomic, assign) size_t tweakSize;
+
+/** numRounds */
+@property (nonatomic, assign) int numRounds;
+
+
+/** CCModeOptions如kCCModeOptionCTR_LE，kCCModeOptionCTR_BE */
+@property (nonatomic, assign) CCModeOptions options;
+
 
 
 // 设置密钥的长度位数
@@ -51,7 +65,7 @@ enum {
     kMLKeySizeMinBlowfish     = 8,
     kMLKeySizeMaxBlowfish     = 56,
 };
-typedef int MLKeySize;
+typedef size_t MLKeySize;
 
 // 设置iv向量的长度位数
 enum {
@@ -86,8 +100,26 @@ enum {
 };
 typedef uint32_t ivMode;
 
-
-//- (NSData *)test_cryptorCommonMethodWihtData:(NSData *)textData Operation:(CCOperation)op Mode:(CCMode)mode Algorithm:(CCAlgorithm)alg Padding:(CCPadding)padding iv:(const void *)iv key:(const void *)key keySize:(int)keySize tweak:(const void *)tweak tweakLength:(size_t)tweakLength numRounds:(int)numRounds ModeOptions:(CCModeOptions)options cryptor:(CCCryptorRef)cryptor;
+/**
+ *  封装的一个多参数的通用的加密解密方法
+ *
+ *  @param textData    传入数据Data
+ *  @param op          加密或解密
+ *  @param mode        加密模式，如CBC,EBC等
+ *  @param alg         加密方式，如AES,DES等
+ *  @param padding     填充方式，如ccPKCS7Padding等
+ *  @param iv          加密向量
+ *  @param key         加密密钥
+ *  @param keySize     密钥长度如，kMLKeySizeAES128等
+ *  @param tweak       在XTS模式才有，「其它模式默认为NULL就好」
+ *  @param tweakLength 在XTS模式才有，密钥长度tweakLength等于keySize「其它模式默认为0就好」
+ *  @param numRounds   圆形的数字密码使用，默认为0就好
+ *  @param options     仅在大的存储模式支持，如CTR模式。「其它默认为0」
+ *  @param cryptor     CCCryptorRef类型
+ *
+ *  @return 加密或解密后的数据Data
+ */
+- (NSData *)cryptorAllParasCommonMethodWihtData:(NSData *)textData Operation:(CCOperation)op Mode:(CCMode)mode Algorithm:(CCAlgorithm)alg Padding:(CCPadding)padding iv:(const void *)iv key:(const void *)key keySize:(MLKeySize)keySize tweak:(const void *)tweak tweakLength:(MLKeySize)tweakLength numRounds:(int)numRounds ModeOptions:(CCModeOptions)options;
 
 - (NSData *)cryptorCommonMethodWihtData:(NSData *)textData MLOperation:(CCOperation)op Algorithm:(CCAlgorithm)alg iv:(const void *)iv key:(const void *)key keySize:(MLKeySize)keySize kCCBlockSize:(MLIvSize)kCCBlockSize;
 
--- MLCryptionDemo/MLCryptionDemo/MLCryption/MLCryptionCommon.m
+++ MLCryptionDemo/MLCryptionDemo/MLCryption/MLCryptionCommon.m
@@ -7,12 +7,29 @@
 //
 
 #import "MLCryptionCommon.h"
-//#import <CommonCrypto/CommonCryptor.h>
-
 
 @implementation MLCryptionCommon
-
-- (NSData *)cryptorCommonMethodWihtData:(NSData *)textData Operation:(CCOperation)op Mode:(CCMode)mode Algorithm:(CCAlgorithm)alg Padding:(CCPadding)padding iv:(const void *)iv key:(const void *)key keySize:(int)keySize tweak:(const void *)tweak tweakLength:(size_t)tweakLength numRounds:(int)numRounds ModeOptions:(CCModeOptions)options cryptor:(CCCryptorRef)cryptor
+CCCryptorRef cryptor = NULL;
+/**
+ *  封装的一个多参数的通用的加密解密方法
+ *
+ *  @param textData    传入数据Data
+ *  @param op          加密或解密
+ *  @param mode        加密模式，如CBC,EBC等
+ *  @param alg         加密方式，如AES,DES等
+ *  @param padding     填充方式，如ccPKCS7Padding等
+ *  @param iv          加密向量
+ *  @param key         加密密钥
+ *  @param keySize     密钥长度如，kMLKeySizeAES128等
+ *  @param tweak       在XTS模式才有，「其它模式默认为NULL就好」
+ *  @param tweakLength 在XTS模式才有，密钥长度tweakLength等于keySize「其它模式默认为0就好」
+ *  @param numRounds   圆形的数字密码使用，默认为0就好
+ *  @param options     仅在大的存储模式支持，如CTR模式。「其它默认为0」
+ *  @param cryptor     CCCryptorRef类型
+ *
+ *  @return 加密或解密后的数据Data
+ */
+- (NSData *)cryptorAllParasCommonMethodWihtData:(NSData *)textData Operation:(CCOperation)op Mode:(CCMode)mode Algorithm:(CCAlgorithm)alg Padding:(CCPadding)padding iv:(const void *)iv key:(const void *)key keySize:(MLKeySize)keySize tweak:(const void *)tweak tweakLength:(MLKeySize)tweakLength numRounds:(int)numRounds ModeOptions:(CCModeOptions)options
 {
     //1.创建加密器CCCryptorRef
     CCCryptorStatus cryptorStatus = CCCryptorCreateWithMode(op, mode, alg, padding, iv, key, keySize, tweak, tweakLength, numRounds, options, &cryptor);
@@ -24,7 +41,7 @@
     
     //2.获取输出数据的最大长度
     bufsize = CCCryptorGetOutputLength(cryptor, textData.length, true);
-    char * buf = (char*)malloc(bufsize);
+    char * buf = (char*)malloc(bufsize);//输出outData
     bzero(buf, bufsize);
     
     //3.加密处理
